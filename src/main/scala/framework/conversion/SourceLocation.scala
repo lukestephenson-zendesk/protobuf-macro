5ebf37c755e5ee8d27eaf1b71b29ae25825475ff
Luke Stephenson <lstephenson@zendesk.com>
2025-03-19 17:00:51 +1100

    Minor improvements

--- src/main/scala/framework/conversion/SourceLocation.scala
+++ src/main/scala/framework/conversion/SourceLocation.scala
@@ -3,6 +3,8 @@ package framework.conversion
 opaque type SourceLocation = String
 
 object SourceLocation {
+  // The ${} syntax is known as splicing.  The ' sytnax is known as quoting. Quoting converts the source code into an AST,
+  // and splicing injects the modified AST back into the code.
   inline def apply(inline value: Any): SourceLocation =
     ${ SourceCodeUtil.sourceCode('value) }
     
